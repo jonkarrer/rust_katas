ceba7b6523441a21d791ea628f5da4564287070d
jkarrer <jkarrer@protect247.com>
2022-06-30 15:03:06 -0400

    disemvowel trolls codewars

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust Katas
+
+> Code algorithms using rust.
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+fn disemvowel(comment: &str) -> String {
+    // * identify vowels
+    fn id_vowel(s:&str) -> &str {
+        // * remove each vowel
+        match s {
+            "a" => "",
+            "e" => "",
+            "i" => "",
+            "o" => "",
+            "u" => "",
+            _ => s
+        }
+    }
+     // * Loop through string
+    let str_arr: Vec<&str> = comment.split("").map(|s:&str| id_vowel(s)).collect();
+
+    // * return string
+    return str_arr.join("");
+}
+
+fn main() {
+   println!("{}", disemvowel("Hello World"));
+}
+
+// fn disemvowel(s: &str) -> String {
+//     s.chars()
+//         .filter(|&c| !"aeiou".contains(c.to_ascii_lowercase()))
+//         .collect()
+// }
